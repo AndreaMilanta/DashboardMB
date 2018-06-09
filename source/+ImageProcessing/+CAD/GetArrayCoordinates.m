fb41661293dc517fe47589f59287d999fbc25863
Andrea Milanta <andrea.milanta@gmail.com>
2018-06-09 12:58:54 +0200

    Added packages

---
+++ source/+ImageProcessing/+CAD/GetArrayCoordinates.m
@@ -0,0 +1,14 @@
+% Get Coordinates of Arrays taken from 'cad' images
+
+% -- cad <- b&w schematic image of the plane
+% -- refPos <- position to consider on the array (BR, MR, TR, BC, MC, TC, BL, CL, TL)
+function coords = GetArrayCoordinates(cad, refPos)
+    coords = 0;
+end
+
+% get coordinates identifying the array
+% -- contour <- list of four points representing the border of an array
+% -- refPos <- position to consider on the array (BR, MR, TR, BC, MC, TC, BL, CL, TL)
+function [x, y] = getRefCoord(contour, pos)
+
+end
